55dda55c7931a874ef4b4329430301c124c836e4
Valter Silva <valter.silva@anevis-solutions.com>
2017-03-03 12:43:28 +0100

    Extract required configuration from .kitchen.yml file of 'chef-vault' repository and add it into mine.
    Copy and paste test/fixtures folders.
    
    Load chef-vault-item('secrets', 'dbpassword') and force to it to not use data-bag fallback.

--- cookbooks/kitchen_vault/recipes/default.rb
+++ cookbooks/kitchen_vault/recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+node.normal['chef-vault']['databag_fallback'] = false
+dbpassword = chef_vault_item("secrets", "dbpassword")
+
+log 'message' do
+  message "DB PASSWORD OBJECT RETRIEVED FROM CHEF VAULT: #{dbpassword}"
+  level :info
+end
+
+log 'message' do
+  message "VALUE OF KEY 'id' FROM DB PASSWORD OBJECT: #{dbpassword['id']}"
+  level :info
+end
+
+log 'message' do
+  message "VALUE OF KEY 'auth' FROM DB PASSWORD OBJECT: #{dbpassword['auth']}"
+  level :info
+end
