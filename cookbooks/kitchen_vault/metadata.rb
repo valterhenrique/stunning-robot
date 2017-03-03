ce6024f142c337b27207e18aab0a4b37e9e45def
Valter Silva <valter.silva@anevis-solutions.com>
2017-03-03 12:12:36 +0100

    Add as dependency chef-vault cookbook.
    Add .kitchen folder into .gitignore.

--- cookbooks/kitchen_vault/metadata.rb
+++ cookbooks/kitchen_vault/metadata.rb
@@ -6,6 +6,8 @@ description 'Installs/Configures kitchen_vault'
 long_description 'Installs/Configures kitchen_vault'
 version '0.1.0'
 
+depends 'chef-vault', '~> 2.1'
+
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
